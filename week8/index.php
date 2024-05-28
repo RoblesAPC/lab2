619260d235b58d0eb2acb8b5866131ae070ef90a
Aljohn Robles <aarobles2@student.apc.edu.ph>
2024-05-28 14:48:07 +0800

    edited .php version

--- week8/index.php
+++ week8/index.php
@@ -6,27 +6,12 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
-
-<script>
-	function showNickname(){
-  document.getElementById("info").innerHTML = "Alj";
-}
-	function showCourse(){
-  document.getElementById("info").innerHTML = "Associate in Computer Technology - Specializing in Full Stack Development";
-}
-	function showScholarship(){
-  document.getElementById("info").innerHTML = "No";
-}
-	function showHobbies(){
-  document.getElementById("info").innerHTML = "Video Games, Football, Hiking, Climbing, Golf, Reading, Scuba Diving, Traveling, Anime, Playing the guitar, Going to the gym, My dog, Programming";
-}
-</script>
 	
 <style>
-body {
+.background {
 	background-image: url("./images/bg_1.png");
 	background-repeat: no-repeat;
-	background-position: center top;
+	background-position: center top left;
 	background-attachment: fixed;
 }
 
@@ -77,17 +62,19 @@ p {
 }
 
 #info {
-	border-style: solid;
 	background: white;
 	color: black;
-	display: flex;
-	margin: auto;
-	width: 50%;
+	position: relative;
+	text-align: left;
+	overflow: auto;
+	width: 400px;
+	height: 175px;
 }
 
 .profilepic {
 	display: inline-flex;
 }
+
 .center {
 	display: flex;
 	justify-content: center;
@@ -138,22 +125,18 @@ p {
 	background-color: #111;
 }
 
-.grow {
-  padding: 5px 5px 5px 5px;
-  border-radius: 10px;
-  height: 49px;
-  width: 22%;
-  margin: 5px 1% 5px 1%;
-  float: left;
-  position: relative;
-  transition: height 0.5s;
-  -webkit-transition: height 0.5s;
-  text-align: center;
+.auto {
+  display: block;
+  width: auto;
+  height: 50px;
   overflow: hidden;
+  transition: all 1s ease;
 }
 
-.grow:hover {
-  height: 145px;
+* {
+	margin: 0;
+	padding: 0;
+	box-sizing: border-box;
 }
 </style>
 </head>
@@ -167,9 +150,10 @@ p {
 	<li style="float:right; border-left:1px solid #bbb;"><a href="#contact">Contact</a></li>
 </ul>
 
+<div class="background">
+<div id="scene" class="scene">
+<div data-depth="0.2" class="layer layer-1">
 <div class="container p-5 w-5 mh-100 bg-dark bg-opacity-75 bg-height-100 text-white" style="height: 100%">
-
-<h1>My Profile Page</h1>
 
 <div class="profilepic">
 	<div class="overlayeffect">
@@ -181,11 +165,12 @@ p {
 
 	<div style="text-align:bottom | left; padding-left: 10px;">
 		<h2>Aljohn Leonel A. Robles</h2>
-		<h5>Fullstack Developer Student</h5>
+		<h5>Fullstack Development Student</h5>
 		<button type="button" onclick="showNickname()">Nickname</button>
 		<button type="button" onclick="showCourse()">Course</button>
 		<button type="button" onclick="showScholarship()">Scholarship</button>
 		<button type="button" onclick="showHobbies()">Hobbies</button>
+		<br>
 		<p id="info">Info will be displayed here</p>
 	</div>
 </div>
@@ -193,39 +178,85 @@ p {
 <br>
 
 <div class="row">
-	<div class="col p-3 bg-dark text-white grow" style="float:center; border-right:1px solid #bbb">Did you choose this course? Why?</div>
-	<div class="col p-3 bg-dark text-white">I.T. Experience/Gaming/Others</div>
-</div>
-
-<div class="row">
-	<div class="col p-3 bg-dark text-white" style="float:center; border-right:1px solid #bbb">Yes I chose this course.I chose this course since, based on my previous educational experience,my brain andthinkingprocessaligns better with a more logicaland structureddiscipline as opposed tomore creative and free-form processes. I also believe that the lifestyle associated with being in the IT industry betteraligns with the kind of life I can foresee for myself in my future.</div>
-	<div class="col p-3 bg-dark text-white">I have been exposed to computers and gaming since I was 5.When I was in Grade School, I would play around with game design applications such as blender and GameMaker.io with some of my closest friends atthe time. When I was in my early twenties, I dabbled a little bit in Shopify web programming due to starting an online e-commerce business during the COVID-19 pandemic.</div>
-</div>
-
-<br>
-
-<div class="row">
-	<div class="col p-3 bg-dark text-white" style="float:center; border-right:1px solid #bbb">What do you expect to learn from this course?</div>
-	<div class="col p-3 bg-dark text-white">What do you want to learn to help you in your chosen Major?</div>
-</div>
-
-<div class="row">
-	<div class="col p-3 bg-dark text-white" style="float:center; border-right:1px solid #bbb">I expect to learn the basicsof web-based programming in order to have a solid foundation to begin upskilling my programming skills in regards to web-based applications and systems.</div>
-	<div class="col p-3 bg-dark text-white">I want tolearn the processes to be able to design aworking softwareapplication all on my own, from conceptualization,to launching saidapplication and continuing routine debugging and maintenance.</div>
+	<div class="col p-3 bg-dark text-white auto">
+	Did you choose this course? Why?
+	
+	<br><br>
+	
+	Yes I chose this course.I chose this course since, based on my previous educational experience,my brain andthinkingprocessaligns better with a more logicaland structureddiscipline as opposed tomore creative and free-form processes. I also believe that the lifestyle associated with being in the IT industry betteraligns with the kind of life I can foresee for myself in my future.
+	</div>
 </div>
 
 <br>
 
 <div class="row">
-	<div class="col p-3 bg-dark text-white" style="float:center; border-right:1px solid #bbb">Goals in life. What do you wish to accomplish in life?</div>
-	<div class="col p-3 bg-dark text-white" style="float:center; border-right:1px solid #bbb">Do you have a dream that you wish to fulfill? If yes, what is your dream?</div>
-	<div class="col p-3 bg-dark text-white">Any other interesting aspects of your life that you would like to share?</div>
+	<div class="col p-3 bg-dark text-white auto">
+	I.T. Experience/Gaming/Others
+	
+	<br><br>
+	
+	I have been exposed to computers and gaming since I was 5.When I was in Grade School, I would play around with game design applications such as blender and GameMaker.io with some of my closest friends atthe time. When I was in my early twenties, I dabbled a little bit in Shopify web programming due to starting an online e-commerce business during the COVID-19 pandemic.
+	</div>
 </div>
 
+<br>
+
 <div class="row">
-	<div class="col p-3 bg-dark text-white" style="float:center; border-right:1px solid #bbb">I want to be able to reach financial freedom and create something which I can call my own whether that be a business venture, or a company of my own.</div>
-	<div class="col p-3 bg-dark text-white" style="float:center; border-right:1px solid #bbb">My dream is to be able to provide back to my communityandsupport those who require aid more than myself either by providing a service whichpeople need, or creating an experience which people seek.I wish to one daymake as many ofthe different hobbies and interest I haveeasily accessibleto others and share the fun andjoy I garner from all the differenthobbies I have inthe blessed life I live.</div>
-	<div class="col p-3 bg-dark text-white">I believe that Ihave struggled with my educational life in the past but have finally reached self-actualization within that regard through my decision to pursue Computer Science and the IT field.This is the first time in my life that I have had a GPA above 3.5 and believe that I have finally found a field I can say I’m confident in and would like to pursue and complete as best I can.</div>
+	<div class="col p-3 bg-dark text-white auto">
+	What do you expect to learn from this course?
+	
+	<br><br>
+	
+	I expect to learn the basicsof web-based programming in order to have a solid foundation to begin upskilling my programming skills in regards to web-based applications and systems.
+	</div>
+</div>
+
+<br>
+
+<div class="row">
+	<div class="col p-3 bg-dark text-white auto">
+	What do you want to learn to help you in your chosen Major?
+	
+	<br><br>
+	
+	I want tolearn the processes to be able to design aworking softwareapplication all on my own, from conceptualization,to launching saidapplication and continuing routine debugging and maintenance.
+	</div>
+</div>
+
+<br>
+
+<div class="row">
+	<div class="col p-3 bg-dark text-white auto">
+	Goals in life. What do you wish to accomplish in life?
+	
+	<br><br>
+	
+	I want to be able to reach financial freedom and create something which I can call my own whether that be a business venture, or a company of my own.
+	</div>
+</div>
+
+<br>
+
+<div class="row">
+	<div class="col p-3 bg-dark text-white auto">
+	Do you have a dream that you wish to fulfill? If yes, what is your dream?
+	
+	<br><br>
+	
+	My dream is to be able to provide back to my communityandsupport those who require aid more than myself either by providing a service whichpeople need, or creating an experience which people seek.I wish to one daymake as many ofthe different hobbies and interest I haveeasily accessibleto others and share the fun andjoy I garner from all the differenthobbies I have inthe blessed life I live.
+	</div>
+</div>
+
+<br>
+
+<div class="row">
+	<div class="col p-3 bg-dark text-white auto">
+	Any other interesting aspects of your life that you would like to share?
+	
+	<br><br>
+	
+	I believe that Ihave struggled with my educational life in the past but have finally reached self-actualization within that regard through my decision to pursue Computer Science and the IT field.This is the first time in my life that I have had a GPA above 3.5 and believe that I have finally found a field I can say I’m confident in and would like to pursue and complete as best I can.
+	</div>
 </div>
 
 <br>
@@ -241,6 +272,48 @@ echo "My first PHP script!";
 ?>
 
 </div>
+</div>
+</div>
+</div>
+
+<script>
+	function showNickname(){
+  document.getElementById("info").innerHTML = "Alj";
+}
+	function showCourse(){
+  document.getElementById("info").innerHTML = "Associate in Computer Technology - Specializing in Full Stack Development";
+}
+	function showScholarship(){
+  document.getElementById("info").innerHTML = "No";
+}
+	function showHobbies(){
+  document.getElementById("info").innerHTML = "Video Games, Football, Hiking, Climbing, Golf, Reading, Scuba Diving, Traveling, Anime, Playing the guitar, Going to the gym, My dog, Programming";
+}
+
+if (document.getElementsByClassName("auto")) {
+  var autos = document.getElementsByClassName("auto");
+  for (var i=0; i<autos.length; i++) {
+    autos[i].addEventListener("mouseover", autoOver);
+    autos[i].addEventListener("mouseout", autoOut);
+  }
+}
+
+function autoOver() {
+  this.style.height = this.scrollHeight + "px";
+}
+
+function autoOut() {
+  this.style.height = "50px";
+}
+
+var scene = document.getElementById('scene');
+
+var parallaxInstance = new Parallax(scene);
+
+parallaxInstance.friction(0.2, 0.2);
+</script>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/parallax/3.1.0/parallax.min.js"></script>
 
 </body>
 </html>
